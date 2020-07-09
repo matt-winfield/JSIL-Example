fe22c9142cf83503a4f3df324a718bd50aaba7fe
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-07-09 16:03:36 +0100

    Add FluentValidation classes

---
+++ ExampleJsilCompilation/Validators/CustomerValidator.cs
@@ -0,0 +1,15 @@
+﻿using ExampleJsilCompilation.Classes;
+using FluentValidation;
+
+namespace ExampleJsilCompilation.Validators
+{
+    class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(customer => customer.FirstName).NotNull().NotEmpty();
+            RuleFor(customer => customer.LastName).NotNull().NotEmpty();
+            RuleFor(customer => customer.Id).GreaterThan(0);
+        }
+    }
+}
